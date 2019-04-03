224c246414ff6bb0301b190c52fda958064972af
TungEscanor <trinhtung95@gmail.com>
2019-04-03 18:54:06 +0700

    add

---
+++ Coder_Tokyo/javascript/bai 25 new keyword/bai 1.js
@@ -0,0 +1,19 @@
+/* 
+  Tạo một đối tượng student có thông tin như sau:
+  Tên: Herry,
+  Giới tính: Nam,
+  Tuổi: 18
+  
+  Viết hàm showInfo để in ra các thông tin của đối tượng đó.
+*/
+
+var student = {
+  name: 'Herry',
+  gender: 'male',
+  age: 18,
+}
+function showInfo(){
+  console.log(student);
+}
+
+showInfo(); // In ra tất cả thông tin của đối tượng
---
+++ Coder_Tokyo/javascript/bai 25 new keyword/bai 4.js
@@ -0,0 +1,23 @@
+/* 
+  Viết hàm xây dựng Shape để tạo các đối tượng hình học có các thuộc tính sau:
+    {
+      color: String,
+      shape: String,
+      fill: boolean
+    }
+*/
+
+function Shape(color, shape, fill) {
+    // Your code here
+  }
+  
+  var shape1 = new Shape("red", "rectangle", true);
+  var shape2 = new Shape("orange", "triangle", false);
+  var shape3 = new Shape("tomato", "circle", true);
+  
+  console.log(shape1, shape2, shape3);
+  /* 
+    Shape { color: 'red', shape: 'rectangle', fill: true } 
+    Shape { color: 'orange', shape: 'triangle', fill: false } 
+    Shape { color: 'tomato', shape: 'circle', fill: true }
+  */
