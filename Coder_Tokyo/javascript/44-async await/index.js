f62330b17b2dd8593dffabf19ffeb821398e2e97
TungEscanor <trinhtung95@gmail.com>
2019-04-14 08:38:10 +0700

    add

--- Coder_Tokyo/javascript/44-async await/index.js
+++ Coder_Tokyo/javascript/44-async await/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 function readFilePromise(path) {
     return new Promise(function(resolve, reject) {
-        fs.readFile(path, 'utf8', function(err, data) {
+        fs.readFile(path, {encoding:'utf8'}, function(err, data) {
             if (err) {
                 reject(err);
             } else {
