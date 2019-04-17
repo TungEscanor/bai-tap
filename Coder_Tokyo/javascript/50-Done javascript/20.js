be0f293847293c8369dd5c7797891ae8c7bbcc33
TungEscanor <trinhtung95@gmail.com>
2019-04-17 17:50:24 +0700

    add

--- Coder_Tokyo/javascript/50-Done javascript/20.js
+++ Coder_Tokyo/javascript/50-Done javascript/20.js
@@ -13,4 +13,10 @@ Output: 8 (là hiệu của 9 và 1)
 
 function findmaxDiff(arr){
     // Viết hàm tại đây
+    var sort = arr.sort((a, b) => a - b);
+    var diffmax = sort[sort.length-1] - sort[0]
+    return diffmax
   }
+
+  var test = findmaxDiff([1,3,2,9,8])
+  console.log(test);
