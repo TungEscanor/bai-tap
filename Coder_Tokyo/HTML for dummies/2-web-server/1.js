1b7c379980b950d996d19f6d53ad00284a71e5af
TungEscanor <trinhtung95@gmail.com>
2019-04-18 18:38:17 +0700

    ađ

---
+++ Coder_Tokyo/HTML for dummies/2-web-server/1.js
@@ -0,0 +1,9 @@
+/**
+ * 1. Ngoài static-server thì còn có những node package nào có thể tạo 1 web server
+ * Gợi ý: Lên https://www.npmjs.com/ search với từ khoá web server hoặc static server hoặc server
+ * 2. Thử dùng 1 vài công cụ khác static-server để tạo web server
+ * 3. Ghi lại các cách mà bạn đã làm ở bước 2, càng cụ thể càng tốt.
+ * 4. Bạn có biết cách start 1 web server rất nhanh với dòng lệnh như php hoặc python, thử tìm hiểu.
+ * 5. Ghi lại cách mà bạn tìm kiếm ở bước 4
+ * Mục đích của bài tập này giúp cho bạn tập tìm kiếm tài liệu với từ khoá đã biết
+ */
