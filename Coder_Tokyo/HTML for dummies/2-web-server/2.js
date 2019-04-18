285d86de5c395568683841aa6075883dd308ae82
TungEscanor <trinhtung95@gmail.com>
2019-04-18 23:31:13 +0700

    add

--- Coder_Tokyo/HTML for dummies/2-web-server/2.js
+++ Coder_Tokyo/HTML for dummies/2-web-server/2.js
@@ -3,3 +3,6 @@
  * Ghi lại địa chỉ private IP của bạn (địa chỉ mà những người trong cùng mạng với bạn ở nơi bạn đang ở có thể thấy và truy cập)
  * Gợi ý: Xem video để biết cách tìm
  */
+
+ // My public IP: 113.178.22.107
+ // My local IP: 192.168.2.2
