43cd6545f15eae1fc3ecef1589cd7357bc18b02d
TungEscanor <trinhtung95@gmail.com>
2019-04-27 16:24:25 +0700

    add

--- Coder_Tokyo/javascript/39-sync-async/2.js
+++ Coder_Tokyo/javascript/39-sync-async/2.js
@@ -9,18 +9,17 @@ var url1 = 'https://jsonplaceholder.typicode.com/todos/1';
 var url2 = 'https://jsonplaceholder.typicode.com/todos/2';
 
 function getData() {
-  var a = axios.get(url1)
+axios.get(url1)
   .then(function(response){
     console.log(response.data);
   }).catch(function(err){
     console.log(err);
   });
-  var b = axios.get(url2)
+axios.get(url2)
   .then(function(response){
     console.log(response.data);
   }).catch(function(err){
     console.log(err);
   });
-  return a,b;
 }
 getData()
